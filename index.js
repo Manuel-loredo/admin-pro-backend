0cb8502aa0261ff8540d378cd0237086cee8d576
Jesus Manuel Loredo Sauceda <manuel.lorsau@gmail.com>
2021-02-02 16:07:31 -0700

    CORS Y Express - Primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors')
+
+const { dbConnection } = require('./database/config');
+
+//crear el servidor de express
+const app = express();
+
+//configurar CORS
+app.use(cors());
+
+//Base de datos
+dbConnection();
+
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    });
+
+});
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en el puerto ' + process.env.PORT);
+});  
